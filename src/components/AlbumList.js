3133e0d49d7fbe00383249adc49f13b35db254e2
Enrico Freni <enrico.freni@temera.it>
2019-07-09 16:31:08 +0200

    aggiunti i 3 step per l'inserimento della get nell'array albums. sostituito WILLmount con DIDmount, visto che will è diventato deprecated e a breve non sarà più suppotato

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -10,13 +10,20 @@ import axios from 'axios'
 //     );
 // }; // sostituzione functional component to class component
 class AlbumList extends Component {
-  /*debugger, key per stoppare il codice e fare il debugging in automatico */
-  componentWillMount() {
+  state = { albums: [] }
+  //1mo step: stato vuoto
+  //debugger, key per stoppare il codice e fare il debugging in automatico
+  componentDidMount() {
     //axios sostitutisce la fetch, rendendola più facile da scrivere
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response))
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }))
+    //secondo step, passiamo l'oggetto all'array albums(response.data è il modo con cui chrome ci dà i dati)
+    //this.setState -> setState è una funzione che è implementata automanticamente dalla classe Component -> funzione di aggiornare il component "ehi, ecco qui dei dati, prendili"
   }
 
   render() {
+    console.log(this.state)
     return (
       <View>
         <Text>Album List</Text>
