6173c55846eec240ee6b049c9443ea1ab8474c2c
Enrico Freni <enrico.freni@temera.it>
2019-07-11 10:02:03 +0200

    Aggiunto key per debugging e prestazioni, creato AlbumDetail e tolto text nell'AlbumList
    Lo scopo è quello di creare un componente che mostir tutti i dettagli invece di un text su AlbumList

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,10 @@
+//serve a mostrare i dettagli di tutti gli album
+
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const AlbumDetail = () => {
+
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import axios from 'axios'
+import AlbumDetail from './AlbumDetail';
 
 // const AlbumList = () => {
 //     return (
@@ -21,9 +22,13 @@ class AlbumList extends Component {
     //secondo step, passiamo l'oggetto all'array albums(response.data è il modo con cui chrome ci dà i dati)
     //this.setState -> setState è una funzione che è implementata automanticamente dalla classe Component -> funzione di aggiornare il component "ehi, ecco qui dei dati, prendili"
   }
-
+  //tolto text e sostituito con albumdetail
+  //key property aggiunta per risolvere il debugging, la key property dev'essere unica e serve a migliorare le performance)
   renderAlbums(){
-    return this.state.albums.map(album => <Text>{album.title}</Text>);
+    return this.state.albums.map(album => 
+    <AlbumDetail key={album.title} album={album}/>
+    
+    );
   }
 
   render() {
