c3fd76c9d4ffb7bae9031870c75a006cf348f046
Enrico Freni <enrico.freni@temera.it>
2019-07-11 17:41:37 +0200

    Invio fine giornata
    Eccolo!

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,16 +5,18 @@ import { Text, View, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album
+
   return (
     <Card>
       <CardSection>
         <View>
-          <Image />
+          <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -25,6 +27,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
   },
 }
 
